53cb3426b140a04e5426e675c138d818ce05efc0
sejoonkim <sejoondev@gmail.com>
2020-09-10 14:37:01 +0900

    feat: Lecture 3 - typescript compiler config

---
+++ dist/hello.js
@@ -0,0 +1,13 @@
+"use strict";
+// var hello = "hello";
+// let hello2 = "hello2";
+// let timeoutPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("1 sec");
+//   }, 1000);
+// });
+// timeoutPromise.then(console.log);
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = require("./util");
+var value = util_1.default(1, 2);
+console.log(value);
